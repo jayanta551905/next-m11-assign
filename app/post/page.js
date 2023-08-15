85f49bf47ae98c3381e77273345e57a1b0c12bba
Jayanta Mondal <jayanta551905@gmail.com>
2023-08-15 22:50:52 +0600

    router

--- app/post/page.js
+++ app/post/page.js
@@ -1,18 +1,10 @@
 "use client";
-import { useRouter } from "next/router";
 import blogData from "../data/blogData.json";
 
 export default function Post() {
-  const router = useRouter();
-  const { id } = router.query;
-  const post = blogData.find((post) => post.id.toString() === id);
-
   return (
     <div className="text-center">
       <h1 className="font-bold text-2xl">Post page</h1>
-      <h2>{post.title}</h2>
-      <p>{post.content}</p>
-      <p>{post.date}</p>
     </div>
   );
 }
